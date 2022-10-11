d483dfd7dd545eaeac22775706c9ad0b4b152954
HollyCeolin <103793775+HollyCeolin@users.noreply.github.com>
2022-10-10 20:50:32 -0500

    params fixed

--- controller.js
+++ controller.js
@@ -55,7 +55,7 @@ module.exports = {
     },
 
     deleteDogs: (req, res) => {
-        //const {id}= req.params;
+        
         const dog_delete = dog_info.find((dog_obj) => dog_obj.id === parseInt(req.params.id));
         dog_info.splice(req.params.id -1, 1);
         res.status(200).json({message: "successful", dog_info});
